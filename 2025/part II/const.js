ccc5039ac54addc754a986a354a1552b6393bb6f
RastkoD <raleliverpool@gmail.com>
2025-06-03 23:42:42 +0200

    const and functions + calc task - done

---
+++ 2025/part II/const.js
@@ -0,0 +1,11 @@
+let name = "rastko";
+const realName = "rastko";
+// const - Int, String, Float/Decimal, Boolean can't be changed
+
+const cars = ["audi", "bmw"];
+// const - array, object can be changed => cars.push
+
+name = "Rale";
+realName = "Rale";
+
+console.log(name, realName);
