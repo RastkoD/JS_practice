eef1c2abcec1997cce23c5ed3344dda6b6f0fec8
RastkoD <raleliverpool@gmail.com>
2025-06-03 18:58:46 +0200

    a bit of JS practice

---
+++ 2025/arrays.js
@@ -0,0 +1,9 @@
+let products = ["apple", "passion fruit", "banana", "grapes"];
+console.log(products[0], products[3]);
+
+products[0] = "orange";
+
+console.log(products[0]);
+
+products.push("pineapple");
+console.log(products, products.length);
---
+++ 2025/script.js
@@ -0,0 +1,13 @@
+let name = "Rastko";
+let age = 36;
+let student = false;
+
+console.log(name, age, student);
+
+let lastName = "Dimitrijevic";
+
+console.log(name);
+
+let fullName = name + " " + lastName;
+
+console.log(fullName);
