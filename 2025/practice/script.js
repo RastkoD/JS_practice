d1992405283d9437137e19ab4e6d3dd9f4f0665b
RastkoD <raleliverpool@gmail.com>
2025-06-04 19:12:31 +0200

    change clr on hover using js - done

---
+++ 2025/practice/script.js
@@ -0,0 +1,30 @@
+const colours = [
+  "#DC143C", // Crimson
+  "#40E0D0", // Turquoise
+  "#FFD700", // Gold
+  "#6A5ACD", // SlateBlue
+  "#228B22", // ForestGreen
+  "#FF7F50", // Coral
+  "#FF1493", // DeepPink
+  "#191970", // MidnightBlue
+  "#FF6347", // Tomato
+  "#FF8C00", // DarkOrange
+  "#8A2BE2", // BlueViolet
+  "#00CED1", // DarkTurquoise
+  "#ADFF2F", // GreenYellow
+  "#FF4500", // OrangeRed
+  "#9370DB", // MediumPurple
+  "#00FA9A", // MediumSpringGreen
+  "#FF69B4", // HotPink
+  "#20B2AA", // LightSeaGreen
+  "#87CEEB", // SkyBlue
+  "#B22222", // FireBrick
+];
+
+const colorChangeDiv = document.getElementById("clrOnHover");
+
+colorChangeDiv.addEventListener("mouseover", () => {
+  const randomClr = colours[Math.floor(Math.random() * colours.length)];
+
+  colorChangeDiv.style.backgroundColor = randomClr;
+});
